1411439af3d3f6014726dd914458f36322de4cb7
Deshan Lanka <deshan.lanka@ncinga.net>
2025-06-27 22:18:44 +0530

    normal for loop

--- go-programming/Tiketbokig.go
+++ go-programming/Tiketbokig.go
@@ -6,48 +6,51 @@ func main() {
 	eventName := "Concert"
 	const eventTikets int = 50
 	var remainingEventTikets uint = 50
+	// var bookings = [50]string{"john doe","jane smith","alice johnson", "bob brown", "charlie green", "dave white", "eve black", "frank gray", "george blue", "harry red",}
+	// var bookings [50]string 
+	var bookings []string
 
 	fmt.Printf("Welcome to the %v event!\n", eventName)
 	fmt.Printf("We got total of %d tickets available.\n", eventTikets)
 	fmt.Printf("Get your event Pasese today before they sold out")
 
 
-	// var bookings = [50]string{"john doe","jane smith","alice johnson", "bob brown", "charlie green", "dave white", "eve black", "frank gray", "george blue", "harry red",}
-	// var bookings [50]string 
-	var bookings []string
+	for{
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTikets uint
+		var firstName string
+		var lastName string
+		var email string
+		var userTikets uint
 
 
-	fmt.Print("Enter your first name: ")
-	fmt.Scanln(&firstName)
+		fmt.Print("Enter your first name: ")
+		fmt.Scanln(&firstName)
 
-	fmt.Print("Enter your last name: ")
-	fmt.Scanln(&lastName)
+		fmt.Print("Enter your last name: ")
+		fmt.Scanln(&lastName)
 
-	fmt.Print("Enter your email: ")
-	fmt.Scanln(&email)
+		fmt.Print("Enter your email: ")
+		fmt.Scanln(&email)
 
-	fmt.Print("Enter number of tickets: ")
-	fmt.Scanln(&userTikets)
+		fmt.Print("Enter number of tickets: ")
+		fmt.Scanln(&userTikets)
 
-	remainingEventTikets =  remainingEventTikets - userTikets
-	// bookings[0] = firstName + " " + lastName
-	/// Use of Dynamic list, set the "var bookings [50]string" the size to "[ ]" this
-	bookings = append(bookings, firstName + " " + lastName)
+		remainingEventTikets =  remainingEventTikets - userTikets
+		// bookings[0] = firstName + " " + lastName
+		/// Use of Dynamic list, set the "var bookings [50]string" the size to "[ ]" this
+		bookings = append(bookings, firstName + " " + lastName)
 
-	fmt.Print("Thank you for your purchase, ", firstName, " ", lastName, "!\n")
-	fmt.Printf("A confirmation email has been sent to %s.\n", email)
-	fmt.Printf("You have successfully purchased %d tickets for the %v event.\n", userTikets, eventName)
-	fmt.Printf("Enjoy the event and have a great time!\n")
-///////////// Array Info ///////////////////
-	fmt.Printf("The total num of bookings so far:%s\n", bookings)
-	fmt.Printf("the first of the slice%v\n", bookings[0])
-	fmt.Printf("The bookig slice type%T\n", bookings)
-	fmt.Printf("Booking slice length:%v\n", len(bookings))
-////////////////////////////////////////////////////////////
-	fmt.Printf("Remaining tickets for the %v event: %d\n", eventName, remainingEventTikets)
+		fmt.Print("Thank you for your purchase, ", firstName, " ", lastName, "!\n")
+		fmt.Printf("A confirmation email has been sent to %s.\n", email)
+		fmt.Printf("You have successfully purchased %d tickets for the %v event.\n", userTikets, eventName)
+		fmt.Printf("Enjoy the event and have a great time!\n")
+	///////////// Array Info ///////////////////
+		fmt.Printf("The total num of bookings so far:%s\n", bookings)
+		fmt.Printf("the first of the slice%v\n", bookings[0])
+		fmt.Printf("The bookig slice type%T\n", bookings)
+		fmt.Printf("Booking slice length:%v\n", len(bookings))
+	////////////////////////////////////////////////////////////
+		fmt.Printf("Remaining tickets for the %v event: %d\n", eventName, remainingEventTikets)
+	}
+	
 }
