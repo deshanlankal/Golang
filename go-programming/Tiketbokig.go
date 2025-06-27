bd9b7533674e9b59cdf0caa0e68a87a2f10ca17c
Deshan Lanka <deshan.lanka@ncinga.net>
2025-06-27 21:54:32 +0530

    Refactor booking system to use a dynamic slice instead of a fixed array for ticket bookings

--- go-programming/Tiketbokig.go
+++ go-programming/Tiketbokig.go
@@ -13,7 +13,8 @@ func main() {
 
 
 	// var bookings = [50]string{"john doe","jane smith","alice johnson", "bob brown", "charlie green", "dave white", "eve black", "frank gray", "george blue", "harry red",}
-	var bookings [50]string 
+	// var bookings [50]string 
+	var bookings []string
 
 	var firstName string
 	var lastName string
@@ -34,7 +35,9 @@ func main() {
 	fmt.Scanln(&userTikets)
 
 	remainingEventTikets =  remainingEventTikets - userTikets
-	bookings[0] = firstName + " " + lastName
+	// bookings[0] = firstName + " " + lastName
+	/// Use of Dynamic list, set the "var bookings [50]string" the size to "[ ]" this
+	bookings = append(bookings, firstName + " " + lastName)
 
 	fmt.Print("Thank you for your purchase, ", firstName, " ", lastName, "!\n")
 	fmt.Printf("A confirmation email has been sent to %s.\n", email)
@@ -42,9 +45,9 @@ func main() {
 	fmt.Printf("Enjoy the event and have a great time!\n")
 ///////////// Array Info ///////////////////
 	fmt.Printf("The total num of bookings so far:%s\n", bookings)
-	fmt.Printf("the first of the array%v\n", bookings[0])
-	fmt.Printf("The bookig array type%T\n", bookings)
-	fmt.Printf("Booking array length%v\n", len(bookings))
+	fmt.Printf("the first of the slice%v\n", bookings[0])
+	fmt.Printf("The bookig slice type%T\n", bookings)
+	fmt.Printf("Booking slice length:%v\n", len(bookings))
 ////////////////////////////////////////////////////////////
 	fmt.Printf("Remaining tickets for the %v event: %d\n", eventName, remainingEventTikets)
 }
